6c774b70629445964b7c2a07beb280fe7f34e187
sarrit <srini.sarrit@gmail.com>
2024-01-09 17:22:07 +0530

    Update image_margin.py

--- image_margin.py
+++ image_margin.py
@@ -34,8 +34,31 @@ img = setMargin(img, margin_top_mm=0, margin_right_mm=10)
 
 
 from scipy.ndimage import shift
-from PIL import Image
+from PIL import Image, ImageOps
 
-img_shifted = shift(img, (10, 0, 0), mode='constant' , cval=255)
-img = Image.fromarray(img_shifted)
+def imageResize(img, margin_top_mm=0, margin_right_mm=0):
+    left,top,right,bottom = 0,0,0,0
+    margin_top_pix = int(margin_top_mm)
+    margin_right_pix = int(margin_right_mm)
+    
+    if(margin_top_pix < 0):
+        right = margin_top_pix
+    else:
+        left = margin_top_pix
+        
+    if(margin_right_pix < 0):
+        bottom = margin_right_pix
+    else:
+        top = margin_right_pix
+        
+    print(top)
+    
+    image_reshape = ImageOps.expand(img, border=(left,top,right,bottom), fill=(255, 255, 255, 255))
+    img_shifted = shift(image_reshape, (-margin_right_pix, -margin_top_pix, 0), mode='constant', cval=255)
+    img = Image.fromarray(img_shifted)
+    return img
+    
+    
+img = Image.open("env9.jpg")
+img = imageResize(img, margin_top_mm=0, margin_right_mm=50)
 img.save("barcode_new.jpg")
